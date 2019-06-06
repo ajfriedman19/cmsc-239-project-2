9a89d90f0920137a88b4c00cd5563d869f997e07
andrew-friedman <ajfsports1@Andrews-MacBook-Pro-5.local>
2019-06-05 21:39:03 -0500

    readding

--- src/components/showcase-button.js
+++ src/components/showcase-button.js
@@ -1,61 +1,38 @@
-import React, {Component} from 'react';
+// Copyright (c) 2016 - 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
-//import Histogram from 'react-chart-histogram';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-
-import { Histogram, DensitySeries, BarSeries, withParentSize, XAxis, YAxis } from '@data-ui/histogram';
-
-export default class ExampleChart2 extends Component {
-  constructor() {
-    super();
-    this.state = {
-      value: false,
-      keyOfInterest: 'animal'
-    };
-  }
-
-  render() {
-    const {value, keyOfInterest} = this.state;
-    const {data} = this.props;
-   // const preppedData = Object.entries(groupBy(data, keyOfInterest)).map(([key, values]) => {
-    //  return {key, size: values.length};
-    //});
-    const labels = data.map(a => a.index);
-    const rawdata = data.map(a => a.pvalue);
-    const options = { fillColor: 'red', strokeColor: 'black'};
-    const ResponsiveHistogram = withParentSize(({ parentWidth, parentHeight, ...rest}) => (
-  <Histogram
-    width={parentWidth}
-    height={parentHeight}
-    {...rest}
-  />
-))
-
-    return (
-      <div>
-        <ResponsiveHistogram
-          ariaLabel = "test"
-          orientation = 'vertical'
-          cumulative={false}
-          normalized={false}
-          binCount={25}
-          valueAccessor={d => d}
-          binType='numeric'
-          renderTooltip={({ event, datum, data, color }) => (
-          <div>
-            <strong style={{ color }}>{datum.bin0} to {datum.bin1}</strong>
-            <div><strong>count </strong>{datum.count}</div>
-            <div><strong>cumulative </strong>{datum.cumulative}</div>
-            <div><strong>density </strong>{datum.density}</div>
-          </div>
-        )}
-        >
-         <BarSeries
-          animated
-          rawdata={rawdata}
-        />
-        </ResponsiveHistogram>
-      </div>
-    );
-  }
+function ShowcaseButton(props) {
+  const {buttonContent, onClick} = props;
+  return (
+    <button className="showcase-button" onClick={onClick}>
+      {buttonContent}
+    </button>
+  );
 }
+
+ShowcaseButton.PropTypes = {
+  buttonContent: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+export default ShowcaseButton;
