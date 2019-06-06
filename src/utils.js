6a144858653ee63c666d977c188fedfa3a750fe5
Scott Wang <zirenw@cs.uchicago.edu>
2019-06-06 03:38:46 -0500

    done w utils

--- src/utils.js
+++ src/utils.js
@@ -25,3 +25,37 @@ export function create_pairs(data) {
 	console.log(rowObject)
 	return rowObject;
 }
+
+/************************************************************************************************************************
+	aggregateByCommunityAndMonths(data, startMonth, endMonth)
+
+	INPUTS
+	- data: the csv file
+	- startMonth: an integer from 1 to 12, representing the month in 2010 we start aggregating data 
+	- endMonth: an integer from 1 to 12, representing the month in 2010 we stop aggregating data
+
+	RETURNS
+	Dictionary with its keys being community names, and its values being the total kwh consumption of all the buildings
+	in that community, between startMonth and endMonth inclusive.  
+
+	REMARKS
+	Require that startMonth < endMonth. 
+**************************************************************************************************************************/
+
+export function aggregateByCommunityAndMonths(data, startMonth, endMonth) {
+	const monthColumns = ['KWH JANUARY 2010', 'KWH FEBRUARY 2010', 'KWH MARCH 2010', 'KWH APRIL 2010', 'KWH MAY 2010',
+	'KWH JUNE 2010', 'KWH JULY 2010', 'KWH AUGUST 2010', 'KWH SEPTEMBER 2010', 'KWH OCTOBER 2010', 'KWH NOVEMBER 2010', 'KWH DECEMBER 2010'];
+	const kwhByCommunityAndMonths = {};
+	const startIndex = startMonth - 1;
+	const endIndex = endMonth - 1; 
+	for (var i = 0; i < data.length; i ++) {
+		var name = data[i]['COMMUNITY AREA NAME']; 
+		if (!(name in kwhByCommunityAndMonths)) {
+			kwhByCommunityAndMonths[name] = 0;
+		} 
+		for (var j = startIndex; j < endIndex; j ++) {
+			kwhByCommunityAndMonths[name] = kwhByCommunityAndMonths[name] + Number(data[i][monthColumns[j]]);
+		}
+	}
+	return kwhByCommunityAndMonths;
+}
